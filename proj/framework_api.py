515d87716b60baddb8da4e6157cc80cbffa5ad09
sourabh1952 <1952gurjar@gmail.com>
2022-12-12 17:59:38 +0530

    first commit

---
+++ proj/framework_api.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import cv2
+import torch
+from PIL import Image 
+import numpy as np 
+import time
+# from playsound import playsound
+# for html 
+st.markdown("""
+<div >
+Hi, this is our project
+</div>
+
+""", unsafe_allow_html=True)
+
+html_string = """
+            <audio controls autoplay>
+              <source src="https://www.orangefreesounds.com/wp-content/uploads/2022/04/Small-bell-ringing-short-sound-effect.mp3" type="audio/mp3">
+            </audio>
+            """
+
+FRAME_WINDOW = st.image([]) #frame window
+
+
+st.markdown("""<h2 style='text-align: center;'>Here I want to position the camera.</h2>""", unsafe_allow_html=True) #title
+
+run = st.checkbox("Start") #checkbox
+#capture video
+model=torch.hub.load('ultralytics/yolov5', 'custom', path='C:\\Users\\Hp\\Desktop\\projectDS3\\yolov5\\runs\\train\\exp5\\weights\\last.pt',force_reload=True) # weights are stored and accessed using the path given 
+ 
+if run == True: # frame will render 
+    capture_vid=cv2.VideoCapture(0) # capturing the video 
+    capture_vid.set(3,640)
+    capture_vid.set(4,480)
+    while True:
+        ret,frame = capture_vid.read()
+        frame= cv2.cvtColor(frame,cv2.COLOR_BGR2RGB) #convert bgr to rgb format 
+        results=model(frame) # fitting the model 
+        print(results)
+        x=str(results) 
+        a=x[21:27] 
+        
+        if(a=="drowsy"):
+           sound = st.empty()
+           sound.markdown(html_string, unsafe_allow_html=True) 
+           time.sleep(2)  # wait for 2 seconds to finish the playing of the audio
+           sound.empty()  # optionally delete the element afterwards
+        img= np.squeeze(results.render()) # rgb breakdown of image 
+        img= Image.fromarray(img) # rendering the image by combining the three components of color 
+        print(img)
+        FRAME_WINDOW.image(img) # adding the image in frame window after complete processing 
+
+
+
---
+++ proj/training_api.py
@@ -0,0 +1,75 @@
+import uuid #give an unique id to every image here a function used which is uuid1
+import os   #manipulate directory from python  take the image generate by uuid and put it in path img_path
+import time #define the time between capturing between taking two image
+import cv2  #to giving access the camera take default color bgr 
+import torch #module loder which load yoloV5
+import matplotlib.pyplot as plt
+from time import sleep
+import tkvideo as tkv
+import tkinter as tk #it provide a basic GUI  similar to HTML
+from tkinter import *
+import customtkinter as ctk  #similar to css
+import numpy as np
+import pandas as pd
+
+from PIL import Image, ImageTk
+
+
+
+img_path = os.path.join('data', 'images')
+img_labels = ['awake', 'drowsy']
+number_imgs = 20
+
+
+
+api_root=tk.Tk() #framwork of app
+api_root.geometry("640x608")
+api_root.title("Project Accident")
+ctk.set_appearance_mode("dark")
+api_label=tk.Label(api_root, text="Welcome To Project Accident")
+api_label.pack() #sab chizo ko pack kar ra hai
+
+def train_data():
+ camFrame=tk.Frame(height=1280,width=720)
+ camFrame.pack()
+ cam_vid=ctk.CTkLabel(camFrame)
+ cam_vid.pack()
+ capture_vid=cv2.VideoCapture(0) #excess hardware intregated device
+#setting pixel size of image 640X480
+ capture_vid.set(3,640)
+ capture_vid.set(4,480)
+# we have to genrate 20 awake and 20 drowsy image
+ for label in img_labels:
+  print('Image Collection for < >'.format(label))
+  time.sleep(5)#switching time between awake and drowsy
+	    
+  for img_num in range(number_imgs):
+	   
+   print('Image Collection for < >, image number < >'.format(label, img_num))
+					
+   ret, frame = capture_vid.read()#read video from camera and render to it on screen
+	#trasport image to actual memory				
+   imgname = os.path.join(img_path, label+'.'+str(uuid.uuid1())+'.jpg')
+						
+   cv2.imwrite(imgname, frame)#saving the image
+					
+   cv2.imshow('Collection of Images', frame)
+				
+   time.sleep(1)
+				
+   if cv2.waitKey(10) & 0xFF == ord('q'):
+    break
+ capture_vid.release()#close the camera
+ cv2.destroyAllWindows()#destroy tkinter video
+ for label in img_labels:
+  print('Image Collection for < >'.format(label))
+  for img_num in range(number_imgs):
+   print('Image Collection for < >, image number < >'.format(label, img_num))
+   imgname = os.path.join(img_path, label+'.'+str(uuid.uuid1())+'.jpg')
+   print(imgname)
+   
+   
+train_button=tk.Button(api_root,text="Train Model for Drowsiness",command=train_data,padx=6, pady=6, bg='cyan')
+train_button.pack()
+
+api_root.mainloop()   
